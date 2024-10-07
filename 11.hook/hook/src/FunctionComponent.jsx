67fe449f94f4a1c04d7626efae92139de0a42392
attoyibi <muchson@alterra.id>
2024-10-07 15:50:05 +0700

    Update FunctionComponent.jsx

--- 11.hook/hook/src/FunctionComponent.jsx
+++ 11.hook/hook/src/FunctionComponent.jsx
@@ -5,14 +5,24 @@ export default function FunctionComponent() {
         name: 'muchson'
     }
     // pembuuatan variabel
-    const [namaVariabel, setNamaVariabel] = useState(dataUsers);
+    const [namaVariabel, setNamaVariabel] = useState('muchson');
+    const [name, setName] = useState('aini');
+
     const handleClick = () => {
         setNamaVariabel(namaVariabel + 1);
         console.log('handle click');
     }
+
+    const handleName = () => {
+        setName("Khurotul Aini");
+    }
     return (
         <div>FunctionComponent = {namaVariabel}
             <button onClick={handleClick}>Rubah nama variabel</button>
+            <hr />
+            {name}
+            <button onClick={handleName}>Khurotul Aini</button>
         </div>
+
     )
 }
