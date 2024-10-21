22e755552ac4a19fcc6b7895d812f00acf7f1f77
attoyibi <43211768+attoyibi@users.noreply.github.com>
2024-10-21 15:58:27 +0700

    Update App.jsx

--- 13. react form/form/src/App.jsx
+++ 13. react form/form/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from 'react'
-
+import reactImg from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -58,6 +58,8 @@ function App() {
     <>
       <h3>img dari public</h3>
       <img src="img/vite.svg" alt="react img" />
+      <h3>img dari src</h3>
+      <img src={reactImg} alt="react img" />
       <h1>React Form Controlled data ketika live deployment</h1>
       <input name='username' type="text" onChange={handleChange} />
       <h3>Select Your Gender</h3>
